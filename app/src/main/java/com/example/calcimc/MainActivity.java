651ab60c56554c7274ab77208ec3824d0a08e4c4
Diego <101148313+Dalistor@users.noreply.github.com>
2023-06-10 14:11:23 -0300

    add photo

--- app/src/main/java/com/example/calcimc/MainActivity.java
+++ app/src/main/java/com/example/calcimc/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.calcimc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -30,24 +31,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void mathIMC (View view) {
-        float _weigth = Float.parseFloat(weigth.getText().toString());
-        float _heigth = Float.parseFloat(heigth.getText().toString());
+        try {
+            float _weigth = Float.parseFloat(weigth.getText().toString());
+            float _heigth = Float.parseFloat(heigth.getText().toString());
 
-        float result = _weigth / (_heigth * _heigth);
+            float result = _weigth / (_heigth * _heigth);
 
-        if (result < 19) {
-            resultText.setText("Resultado: " + result + " | Abaixo do peso");
-        } else if (result >= 19 && result < 25) {
-            resultText.setText("Resultado: " + result + " | Peso normal");
-        } else if (result >= 25 && result < 30) {
-            resultText.setText("Resultado: " + result + " | Sobrepeso");
-        } else if (result >= 30 && result < 40) {
-            resultText.setText("Resultado: " + result + " | Obesidade tipo I");
-        } else if (result >= 30 && result < 40) {
-            resultText.setText("Resultado: " + result + " | Obesidade tipo II");
-        } else if (result >= 30 && result < 40) {
-            resultText.setText("Resultado: " + result + " | Obesidade mórbida");
+            if (result < 19) {
+                resultText.setText("Resultado: " + result + " | Abaixo do peso");
+            } else if (result >= 19 && result < 25) {
+                resultText.setText("Resultado: " + result + " | Peso normal");
+            } else if (result >= 25 && result < 30) {
+                resultText.setText("Resultado: " + result + " | Sobrepeso");
+            } else if (result >= 30 && result < 35) {
+                resultText.setText("Resultado: " + result + " | Obesidade classe I");
+            } else if (result >= 35 && result < 40) {
+                resultText.setText("Resultado: " + result + " | Obesidade classe II");
+            } else if (result >= 40) {
+                resultText.setText("Resultado: " + result + " | Obesidade classe III");
+            }
+        } catch (Exception e) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder
+                    .setTitle("Campo vazio")
+                    .setPositiveButton("Fechar", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .setMessage("Preencha os campos vazios");
+
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         }
+
     }
 
 
